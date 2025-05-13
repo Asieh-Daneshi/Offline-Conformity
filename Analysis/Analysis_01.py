3b4c17f8fbcbc7591f9b6d52ac6b89868f7a0f8a
Asieh Daneshi <asieh.daneshi@gmail.com>
2025-05-13 11:29:39 +0200

    Code optimized
    to do(s):
    ANOVA or GMM on follow percentage
    response time analysis

--- Analysis/Analysis_01.py
+++ Analysis/Analysis_01.py
@@ -45,23 +45,13 @@ for a1 in np.arange(0,50):
 # -----------------------------------------------------------------------------
 # =============================================================================
 FollowPercentage = np.empty([50,1])  # Follow percentage in main trials
-FollowPercentageCon1 = np.empty([50,1])  # Follow PercentageCon in main trials with 1 agent
-FollowPercentageCon2 = np.empty([50,1])  # Follow PercentageCon in main trials with 2 agent
-FollowPercentageCon3 = np.empty([50,1])  # Follow PercentageCon in main trials with 3 agent
-FollowPercentageCon4 = np.empty([50,1])  # Follow PercentageCon in main trials with 4 agent
-FollowPercentageCon5 = np.empty([50,1])  # Follow PercentageCon in main trials with 5 agent
-FollowPercentageCon6 = np.empty([50,1])  # Follow PercentageCon in main trials with 6 agent
-FollowPercentageCon7 = np.empty([50,1])  # Follow PercentageCon in main trials with 7 agent
-FollowPercentageCon8 = np.empty([50,1])  # Follow PercentageCon in main trials with 8 agent
-
-FollowPercentageIncon1 = np.empty([50,1])  # Follow PercentageIncon in main trials with 1 agent
-FollowPercentageIncon2 = np.empty([50,1])  # Follow PercentageIncon in main trials with 2 agent
-FollowPercentageIncon3 = np.empty([50,1])  # Follow PercentageIncon in main trials with 3 agent
-FollowPercentageIncon4 = np.empty([50,1])  # Follow PercentageIncon in main trials with 4 agent
-FollowPercentageIncon5 = np.empty([50,1])  # Follow PercentageIncon in main trials with 5 agent
-FollowPercentageIncon6 = np.empty([50,1])  # Follow PercentageIncon in main trials with 6 agent
-FollowPercentageIncon7 = np.empty([50,1])  # Follow PercentageIncon in main trials with 7 agent
-FollowPercentageIncon8 = np.empty([50,1])  # Follow PercentageIncon in main trials with 8 agent
+for a1 in np.arange(1,9):
+    Name1 = 'FollowPercentageCon'+str(a1)
+    globals()[Name1] = np.empty([50,1])    # Follow PercentageCon in main trials with a1 agent
+    globals()[Name1][:]=np.nan
+    Name2 = 'FollowPercentageIncon'+str(a1)
+    globals()[Name2] = np.empty([50,1])    # Follow PercentageIncon in main trials with a1 agent
+    globals()[Name2][:]=np.nan
 
 Outliers = np.where(Performance<=0.75)
 for a1 in np.delete(np.arange(0,50),Outliers):
@@ -85,134 +75,43 @@ for a1 in np.delete(np.arange(0,50),Outliers):
     df_Test_Con = df_Test[df_Test['Congruency factor']==1]
     df_Test_Incon = df_Test[df_Test['Congruency factor']==2]
     # -------------------------------------------------------------------------
-    df_Test_Con_Main1 = df_Test_Con[(df_Test_Con['Threshold']==0.5) & (df_Test_Con['NumberOfRespondingAgents']==1)]
-    if len(df_Test_Con_Main1)!=0:
-        FollowPercentageCon1[a1] = len(df_Test_Con_Main1[(df_Test_Con_Main1['Participant raised hand']==1) & (df_Test_Con_Main1['Agents raised hand']==2)|
-                                                  (df_Test_Con_Main1['Participant raised hand']==2) & (df_Test_Con_Main1['Agents raised hand']==1)])/len(df_Test_Con_Main1)
-    # -------------------------------------------------------------------------
-    df_Test_Con_Main2 = df_Test_Con[(df_Test_Con['Threshold']==0.5) & (df_Test_Con['NumberOfRespondingAgents']==2)]
-    if len(df_Test_Con_Main2)!=0:
-        FollowPercentageCon2[a1] = len(df_Test_Con_Main2[(df_Test_Con_Main2['Participant raised hand']==1) & (df_Test_Con_Main2['Agents raised hand']==2)|
-                                                  (df_Test_Con_Main2['Participant raised hand']==2) & (df_Test_Con_Main2['Agents raised hand']==1)])/len(df_Test_Con_Main2)
-    # -------------------------------------------------------------------------
-    df_Test_Con_Main3 = df_Test_Con[(df_Test_Con['Threshold']==0.5) & (df_Test_Con['NumberOfRespondingAgents']==3)]
-    if len(df_Test_Con_Main3)!=0:
-        FollowPercentageCon3[a1] = len(df_Test_Con_Main3[(df_Test_Con_Main3['Participant raised hand']==1) & (df_Test_Con_Main3['Agents raised hand']==2)|
-                                                  (df_Test_Con_Main3['Participant raised hand']==2) & (df_Test_Con_Main3['Agents raised hand']==1)])/len(df_Test_Con_Main3)
-    # -------------------------------------------------------------------------
-    df_Test_Con_Main4 = df_Test_Con[(df_Test_Con['Threshold']==0.5) & (df_Test_Con['NumberOfRespondingAgents']==4)]
-    if len(df_Test_Con_Main4)!=0:
-        FollowPercentageCon4[a1] = len(df_Test_Con_Main4[(df_Test_Con_Main4['Participant raised hand']==1) & (df_Test_Con_Main4['Agents raised hand']==2)|
-                                                  (df_Test_Con_Main4['Participant raised hand']==2) & (df_Test_Con_Main4['Agents raised hand']==1)])/len(df_Test_Con_Main4)
-    # -------------------------------------------------------------------------
-    df_Test_Con_Main5 = df_Test_Con[(df_Test_Con['Threshold']==0.5) & (df_Test_Con['NumberOfRespondingAgents']==5)]
-    if len(df_Test_Con_Main5)!=0:
-        FollowPercentageCon5[a1] = len(df_Test_Con_Main5[(df_Test_Con_Main5['Participant raised hand']==1) & (df_Test_Con_Main5['Agents raised hand']==2)|
-                                                  (df_Test_Con_Main5['Participant raised hand']==2) & (df_Test_Con_Main5['Agents raised hand']==1)])/len(df_Test_Con_Main5)
-    # -------------------------------------------------------------------------
-    df_Test_Con_Main6 = df_Test_Con[(df_Test_Con['Threshold']==0.5) & (df_Test_Con['NumberOfRespondingAgents']==6)]
-    if len(df_Test_Con_Main6)!=0:
-        FollowPercentageCon6[a1] = len(df_Test_Con_Main6[(df_Test_Con_Main6['Participant raised hand']==1) & (df_Test_Con_Main6['Agents raised hand']==2)|
-                                                  (df_Test_Con_Main6['Participant raised hand']==2) & (df_Test_Con_Main6['Agents raised hand']==1)])/len(df_Test_Con_Main6)
-    # -------------------------------------------------------------------------
-    df_Test_Con_Main7 = df_Test_Con[(df_Test_Con['Threshold']==0.5) & (df_Test_Con['NumberOfRespondingAgents']==7)]
-    if len(df_Test_Con_Main7)!=0:
-        FollowPercentageCon7[a1] = len(df_Test_Con_Main7[(df_Test_Con_Main7['Participant raised hand']==1) & (df_Test_Con_Main7['Agents raised hand']==2)|
-                                                  (df_Test_Con_Main7['Participant raised hand']==2) & (df_Test_Con_Main7['Agents raised hand']==1)])/len(df_Test_Con_Main7)
-    # -------------------------------------------------------------------------
-    df_Test_Con_Main8 = df_Test_Con[(df_Test_Con['Threshold']==0.5) & (df_Test_Con['NumberOfRespondingAgents']==8)]
-    if len(df_Test_Con_Main8)!=0:   
-        FollowPercentageCon8[a1] = len(df_Test_Con_Main8[(df_Test_Con_Main8['Participant raised hand']==1) & (df_Test_Con_Main8['Agents raised hand']==2)|
-                                                  (df_Test_Con_Main8['Participant raised hand']==2) & (df_Test_Con_Main8['Agents raised hand']==1)])/len(df_Test_Con_Main8)
-    # -------------------------------------------------------------------------
-    df_Test_Incon_Main1 = df_Test_Incon[(df_Test_Incon['Threshold']==0.5) & (df_Test_Incon['NumberOfRespondingAgents']==1)]
-    if len(df_Test_Incon_Main1)!=0:
-        FollowPercentageIncon1[a1] = len(df_Test_Incon_Main1[(df_Test_Incon_Main1['Participant raised hand']==1) & (df_Test_Incon_Main1['Agents raised hand']==2)|
-                                                  (df_Test_Incon_Main1['Participant raised hand']==2) & (df_Test_Incon_Main1['Agents raised hand']==1)])/len(df_Test_Incon_Main1)
-    # -------------------------------------------------------------------------
-    df_Test_Incon_Main2 = df_Test_Incon[(df_Test_Incon['Threshold']==0.5) & (df_Test_Incon['NumberOfRespondingAgents']==2)]
-    if len(df_Test_Incon_Main2)!=0:
-        FollowPercentageIncon2[a1] = len(df_Test_Incon_Main2[(df_Test_Incon_Main2['Participant raised hand']==1) & (df_Test_Incon_Main2['Agents raised hand']==2)|
-                                                  (df_Test_Incon_Main2['Participant raised hand']==2) & (df_Test_Incon_Main2['Agents raised hand']==1)])/len(df_Test_Incon_Main2)
-    # -------------------------------------------------------------------------
-    df_Test_Incon_Main3 = df_Test_Incon[(df_Test_Incon['Threshold']==0.5) & (df_Test_Incon['NumberOfRespondingAgents']==3)]
-    if len(df_Test_Incon_Main3)!=0:
-        FollowPercentageIncon3[a1] = len(df_Test_Incon_Main3[(df_Test_Incon_Main3['Participant raised hand']==1) & (df_Test_Incon_Main3['Agents raised hand']==2)|
-                                                  (df_Test_Incon_Main3['Participant raised hand']==2) & (df_Test_Incon_Main3['Agents raised hand']==1)])/len(df_Test_Incon_Main3)
-    # -------------------------------------------------------------------------
-    df_Test_Incon_Main4 = df_Test_Incon[(df_Test_Incon['Threshold']==0.5) & (df_Test_Incon['NumberOfRespondingAgents']==4)]
-    if len(df_Test_Incon_Main4)!=0:
-        FollowPercentageIncon4[a1] = len(df_Test_Incon_Main4[(df_Test_Incon_Main4['Participant raised hand']==1) & (df_Test_Incon_Main4['Agents raised hand']==2)|
-                                                  (df_Test_Incon_Main4['Participant raised hand']==2) & (df_Test_Incon_Main4['Agents raised hand']==1)])/len(df_Test_Incon_Main4)
-    # -------------------------------------------------------------------------
-    df_Test_Incon_Main5 = df_Test_Incon[(df_Test_Incon['Threshold']==0.5) & (df_Test_Incon['NumberOfRespondingAgents']==5)]
-    if len(df_Test_Incon_Main5)!=0:
-        FollowPercentageIncon5[a1] = len(df_Test_Incon_Main5[(df_Test_Incon_Main5['Participant raised hand']==1) & (df_Test_Incon_Main5['Agents raised hand']==2)|
-                                                  (df_Test_Incon_Main5['Participant raised hand']==2) & (df_Test_Incon_Main5['Agents raised hand']==1)])/len(df_Test_Incon_Main5)
-    # -------------------------------------------------------------------------
-    df_Test_Incon_Main6 = df_Test_Incon[(df_Test_Incon['Threshold']==0.5) & (df_Test_Incon['NumberOfRespondingAgents']==6)]
-    if len(df_Test_Incon_Main6)!=0:
-        FollowPercentageIncon6[a1] = len(df_Test_Incon_Main6[(df_Test_Incon_Main6['Participant raised hand']==1) & (df_Test_Incon_Main6['Agents raised hand']==2)|
-                                                  (df_Test_Incon_Main6['Participant raised hand']==2) & (df_Test_Incon_Main6['Agents raised hand']==1)])/len(df_Test_Incon_Main6)
-    # -------------------------------------------------------------------------
-    df_Test_Incon_Main7 = df_Test_Incon[(df_Test_Incon['Threshold']==0.5) & (df_Test_Incon['NumberOfRespondingAgents']==7)]
-    if len(df_Test_Incon_Main7)!=0:
-        FollowPercentageIncon7[a1] = len(df_Test_Incon_Main7[(df_Test_Incon_Main7['Participant raised hand']==1) & (df_Test_Incon_Main7['Agents raised hand']==2)|
-                                                  (df_Test_Incon_Main7['Participant raised hand']==2) & (df_Test_Incon_Main7['Agents raised hand']==1)])/len(df_Test_Incon_Main7)
-    # -------------------------------------------------------------------------
-    df_Test_Incon_Main8 = df_Test_Incon[(df_Test_Incon['Threshold']==0.5) & (df_Test_Incon['NumberOfRespondingAgents']==8)]
-    if len(df_Test_Incon_Main8)!=0:   
-        FollowPercentageIncon8[a1] = len(df_Test_Incon_Main8[(df_Test_Incon_Main8['Participant raised hand']==1) & (df_Test_Incon_Main8['Agents raised hand']==2)|
-                                                  (df_Test_Incon_Main8['Participant raised hand']==2) & (df_Test_Incon_Main8['Agents raised hand']==1)])/len(df_Test_Incon_Main8)
+    # calculating follow percentage for all participants
+    for a2 in np.arange(1,9):
+        temp1 = df_Test_Con[(df_Test_Con['Threshold']==0.5) & (df_Test_Con['NumberOfRespondingAgents']==a2)]
+        if len(temp1) != 0:
+            name3 = 'FollowPercentageCon'+str(a2)
+            globals()[name3][a1] = len(temp1[(temp1['Participant raised hand']==1) & (temp1['Agents raised hand']==2)|
+                                                      (temp1['Participant raised hand']==2) & (temp1['Agents raised hand']==1)])/len(temp1)
+        # -------------------------------------------------------------------------
+        temp2 = df_Test_Incon[(df_Test_Incon['Threshold']==0.5) & (df_Test_Incon['NumberOfRespondingAgents']==a2)]
+        if len(temp2) != 0:
+            name4 = 'FollowPercentageIncon'+str(a2)
+            globals()[name4][a1] = len(temp2[(temp2['Participant raised hand']==1) & (temp2['Agents raised hand']==2)|
+                                                      (temp2['Participant raised hand']==2) & (temp2['Agents raised hand']==1)])/len(temp2)
 # =============================================================================
 # Follow percentage plot and ANOVA
+# =============================================================================
+# mean and sem follow percentage across participants
 meanFollowPercentageCon = np.empty([8,1])
-meanFollowPercentageCon[0] = np.nanmean(FollowPercentageCon1)
-meanFollowPercentageCon[1] = np.nanmean(FollowPercentageCon2)
-meanFollowPercentageCon[2] = np.nanmean(FollowPercentageCon3)
-meanFollowPercentageCon[3] = np.nanmean(FollowPercentageCon4)
-meanFollowPercentageCon[4] = np.nanmean(FollowPercentageCon5)
-meanFollowPercentageCon[5] = np.nanmean(FollowPercentageCon6)
-meanFollowPercentageCon[6] = np.nanmean(FollowPercentageCon7)
-meanFollowPercentageCon[7] = np.nanmean(FollowPercentageCon8)
-# -----------------------------------------------------------------------------
 semFollowPercentageCon = np.empty([8,1])
-semFollowPercentageCon[0] = np.nanstd(FollowPercentageCon1)/mt.sqrt(len(FollowPercentageCon1))
-semFollowPercentageCon[1] = np.nanstd(FollowPercentageCon2)/mt.sqrt(len(FollowPercentageCon2))
-semFollowPercentageCon[2] = np.nanstd(FollowPercentageCon3)/mt.sqrt(len(FollowPercentageCon3))
-semFollowPercentageCon[3] = np.nanstd(FollowPercentageCon4)/mt.sqrt(len(FollowPercentageCon4))
-semFollowPercentageCon[4] = np.nanstd(FollowPercentageCon5)/mt.sqrt(len(FollowPercentageCon5))
-semFollowPercentageCon[5] = np.nanstd(FollowPercentageCon6)/mt.sqrt(len(FollowPercentageCon6))
-semFollowPercentageCon[6] = np.nanstd(FollowPercentageCon7)/mt.sqrt(len(FollowPercentageCon7))
-semFollowPercentageCon[7] = np.nanstd(FollowPercentageCon8)/mt.sqrt(len(FollowPercentageCon8))
-# -----------------------------------------------------------------------------
 meanFollowPercentageIncon = np.empty([8,1])
-meanFollowPercentageIncon[0] = np.nanmean(FollowPercentageIncon1)
-meanFollowPercentageIncon[1] = np.nanmean(FollowPercentageIncon2)
-meanFollowPercentageIncon[2] = np.nanmean(FollowPercentageIncon3)
-meanFollowPercentageIncon[3] = np.nanmean(FollowPercentageIncon4)
-meanFollowPercentageIncon[4] = np.nanmean(FollowPercentageIncon5)
-meanFollowPercentageIncon[5] = np.nanmean(FollowPercentageIncon6)
-meanFollowPercentageIncon[6] = np.nanmean(FollowPercentageIncon7)
-meanFollowPercentageIncon[7] = np.nanmean(FollowPercentageIncon8)
-# -----------------------------------------------------------------------------
 semFollowPercentageIncon = np.empty([8,1])
-semFollowPercentageIncon[0] = np.nanstd(FollowPercentageIncon1)/mt.sqrt(len(FollowPercentageIncon1))
-semFollowPercentageIncon[1] = np.nanstd(FollowPercentageIncon2)/mt.sqrt(len(FollowPercentageIncon2))
-semFollowPercentageIncon[2] = np.nanstd(FollowPercentageIncon3)/mt.sqrt(len(FollowPercentageIncon3))
-semFollowPercentageIncon[3] = np.nanstd(FollowPercentageIncon4)/mt.sqrt(len(FollowPercentageIncon4))
-semFollowPercentageIncon[4] = np.nanstd(FollowPercentageIncon5)/mt.sqrt(len(FollowPercentageIncon5))
-semFollowPercentageIncon[5] = np.nanstd(FollowPercentageIncon6)/mt.sqrt(len(FollowPercentageIncon6))
-semFollowPercentageIncon[6] = np.nanstd(FollowPercentageIncon7)/mt.sqrt(len(FollowPercentageIncon7))
-semFollowPercentageIncon[7] = np.nanstd(FollowPercentageIncon8)/mt.sqrt(len(FollowPercentageIncon8))
-x = np.arange(1,9)
-# plt.plot(x.transpose(),meanFollowPercentage)
-# plt.errorbar(x.transpose(),meanFollowPercentage,yerr=semFollowPercentage)
-plt.errorbar(x, meanFollowPercentageCon.flatten(), yerr=semFollowPercentageCon.flatten(), 
-             fmt='o', capsize=5)
-plt.errorbar(x, meanFollowPercentageIncon.flatten(), yerr=semFollowPercentageIncon.flatten(), 
-             fmt='o', capsize=5)
-plt.xlabel('X')
+for a1 in np.arange(0,8):
+    meanFollowPercentageCon[a1] = np.nanmean(globals()['FollowPercentageCon'+str(a1+1)])
+    semFollowPercentageCon[a1] = np.nanstd(globals()['FollowPercentageCon'+str(a1+1)])/mt.sqrt(len(globals()['FollowPercentageCon'+str(a1+1)]))
+    meanFollowPercentageIncon[a1] = np.nanmean(globals()['FollowPercentageIncon'+str(a1+1)])
+    semFollowPercentageIncon[a1] = np.nanstd(globals()['FollowPercentageIncon'+str(a1+1)])/mt.sqrt(len(globals()['FollowPercentageIncon'+str(a1+1)]))
+# =============================================================================
+# bar plot with errorbar
+# =============================================================================
+NumAgents = np.arange(1,9)      # number of responding agents
+plt.bar(NumAgents-0.2,meanFollowPercentageCon.flatten(),width=0.4,facecolor='#219EBC',edgecolor='#023047')
+plt.errorbar(NumAgents-0.2,meanFollowPercentageCon.flatten(), yerr=semFollowPercentageCon.flatten(), 
+             fmt='o', capsize=5, color='#023047')
+plt.bar(NumAgents+0.2,meanFollowPercentageIncon.flatten(),width=0.4,facecolor='#FFB703',edgecolor='#FB8500')
+plt.errorbar(NumAgents+0.2, meanFollowPercentageIncon.flatten(), yerr=semFollowPercentageIncon.flatten(), 
+             fmt='o', capsize=5, color='#FB8500')
+plt.xlabel('Number of Responding Agents')
 plt.ylabel('Mean Follow Percentage')
 plt.title('Mean Follow Percentage with SEM Error Bars')
 plt.show()
